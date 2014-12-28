09c34088b868195d4310b62c0eeae8fdcfb97bfd
Micah Nolte <holler@micahnolte.com>
2014-12-28 15:36:32 -0800

    Links to image pages, and separate view for it.

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -2,20 +2,26 @@ lsohApp.controller('MainController', function(){
 	// Global stuff here?
 })
 
-lsohApp.controller('ImageShowController', function($routeParams){
-	this.image_id = $routeParams.image_id;
+lsohApp.controller('ImageShowController', function($http, $routeParams){
+	var self = this;
+	this.image = {}
+	$http.get('/data/images/' + $routeParams.image_id + '.json', { cache: true }).success( function(response) {
+		self.image = response
+		$http.get('/data/users/' + self.image.user_id + '.json', { cache: true }).success( function(res) {
+			self.image.user = res
+		})
+	})
 })
 
-
-lsohApp.controller('ImageListController', ['$http', function ($http) {
+lsohApp.controller('ImageListController', function ($http) {
 	var self = this
 	this.images = []
 	$http.get('/data/lists/shops-all.json').success( function(response) {
 		self.images = response.images
 	})
-}])
+})
 
-lsohApp.controller('ImageItemController', ['$http', function ($http) {
+lsohApp.controller('ImageItemController', function ($http) {
 	var self = this
 	this.image = {}
 
@@ -27,4 +33,4 @@ lsohApp.controller('ImageItemController', ['$http', function ($http) {
 			})
 		})
 	};
-}])
+})
